f52f92c10cf96af3894c5b01caec01b906d04b56
Steffan Andrews <stef@orchetect.com>
2021-11-01 23:46:12 -0700

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "MIDIKitSMF",
 
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10)
+    ],
 
     products: [
         .library(
@@ -16,10 +18,13 @@ let package = Package(
     ],
 
     dependencies: [
+        // MIDIKit dependencies
         .package(url: "https://github.com/orchetect/MIDIKit", from: "0.2.1"),
+        .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.3"),
+        
+        // MIDIKitSMF dependencies
         .package(url: "https://github.com/orchetect/OTCore", from: "1.1.14"),
         .package(url: "https://github.com/orchetect/SwiftASCII", from: "1.0.1"),
-        .package(url: "https://github.com/orchetect/SwiftRadix", from: "1.0.3"),
         .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.2.3"),
     ],
 
