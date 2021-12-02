7075a6f2647fbed7843588c1c69e91298e143284
Steffan Andrews <stef@orchetect.com>
2021-12-02 03:53:15 -0800

    Bumped OTCore to 1.1.24

---
+++ Sources/MIDIKitSMF/Utilities/Log.swift
@@ -0,0 +1,9 @@
+//
+//  Log.swift
+//  MIDIKitSMF • https://github.com/orchetect/MIDIKitSMF
+//
+
+@_implementationOnly import OTCore
+
+let logger = OSLogger(enabled: true,
+                      useEmoji: .disabled)
